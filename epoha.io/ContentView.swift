fc954fdbbdcaeb403e3eca5c41bac400185485f8
Kostiantyn Nevinchanyi <constantinekos9@gmail.com>
2020-06-03 21:56:22 +0300

    Initial Commit

---
+++ epoha.io/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  epoha.io
+//
+//  Created by constantine kos on 03.06.2020.
+//  Copyright © 2020 constantine kos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
